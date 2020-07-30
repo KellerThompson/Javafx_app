eb64bd060db30611d1d186ab7353a9fa43df06ee
Keller Thompson <kellerthompson_ipn@hotmail.com>
2020-07-30 18:10:01 -0500

    Validacion de TextField en registro usuario

--- src/Vista/Base/UserPane/Register/RegisterController.java
+++ src/Vista/Base/UserPane/Register/RegisterController.java
@@ -3,9 +3,13 @@ package Vista.Base.UserPane.Register;
 import Controlador.model.Persona.PersonaControl;
 import Controlador.model.User.UserControl;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RequiredFieldValidator;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Button;
 
 import javax.swing.*;
+import static Vista.Inicio.frame;
 
 public class RegisterController
 {
@@ -13,24 +17,62 @@ public class RegisterController
                             JFXTextField usernameTextfield, JFXTextField passwordTextfield,
                             Button registerUserButton)
     {
-        registerUserButton.onActionProperty().setValue(event ->
+
+        setValidators(nombreTextfield);
+        setValidators(apellidoTextfield);
+        setValidators(usernameTextfield);
+        setValidators(passwordTextfield);
+
+        registerUserButton.onActionProperty().setValue(event -> {
+            registrar(nombreTextfield, apellidoTextfield, usernameTextfield, passwordTextfield);
+        });
+    }
+
+    private static void registrar(JFXTextField nombreTextfield, JFXTextField apellidoTextfield,
+                                  JFXTextField usernameTextfield, JFXTextField passwordTextfield)
+    {
+
+        try
         {
-            String nombre = nombreTextfield.getText();
-            String apellidos = apellidoTextfield.getText();
-            String username = usernameTextfield.getText();
-            String password = passwordTextfield.getText();
-            try
+            if(nombreTextfield.validate() && apellidoTextfield.validate() && usernameTextfield.validate() && passwordTextfield.validate())
             {
+                String nombre = nombreTextfield.getText();
+                String apellidos = apellidoTextfield.getText();
+                String username = usernameTextfield.getText();
+                String password = passwordTextfield.getText();
+
                 System.out.println(UserControl.registrar(PersonaControl.registrar(nombre, apellidos), username, password).toString());
+
                 nombreTextfield.setText("");
                 apellidoTextfield.setText("");
                 usernameTextfield.setText("");
                 passwordTextfield.setText("");
-                JOptionPane.showMessageDialog(null, "Registro Completo");
             }
-            catch (Exception ex)
+            else
             {
-                JOptionPane.showMessageDialog(null, ex);
+                System.out.println("No valido");
+            }
+        }
+        catch (Exception ex)
+        {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }
+
+    private static void setValidators(JFXTextField textField)
+    {
+        RequiredFieldValidator validator = new RequiredFieldValidator();
+        textField.getValidators().add(validator);
+        validator.setMessage("Campo obligatorio");
+        textField.focusedProperty().addListener(new ChangeListener<Boolean>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue)
+            {
+                if(!newValue)
+                {
+                    textField.validate();
+                }
             }
         });
     }
